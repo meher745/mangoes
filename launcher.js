8193e71bf2e72d0b808a790b04422d0b67b739f9
meher745 <75887880+meher745@users.noreply.github.com>
2021-05-06 13:26:06 +0530

    Add files via upload

--- launcher.js
+++ launcher.js
@@ -1,31 +1,38 @@
 class launcher{
-    constructor(bodyA,pointB){
-        var options={
-            bodyA:bodyA,
-            pointB:pointB,
-            stiffness:3,
-            length:10
-        }
-        this.pointB=pointB;
-        this.launcher=Constraint.create(options);
-        World.add(world,this.launcher);
+	constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
 
-    }
-    attach(body){
-        this.launcher.bodyA=body;
-    }
-    fly(){
-        this.launcher.bodyA=null;
-    }
-    display(){
-        if(this.launcher.bodyA){
-            var pointA=this.launcher.bodyA.position;
-            var pointB=this.pointB;
-            push();
-            stroke("black");
-            strokeWeight(4);
-            line(pointB.x,pointB.y,pointA.x,pointA.y);
-            pop();
-        }
-    }
+	attach(body){
+		this.launcher.bodyA=body;
+	}
+
+	fly()
+	{
+		this.launcher.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(2);		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}
 }
